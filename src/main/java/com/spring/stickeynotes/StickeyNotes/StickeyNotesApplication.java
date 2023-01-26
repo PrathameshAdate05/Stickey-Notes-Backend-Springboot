d8316df0e5ede3875fba1d6b2dc01502cf0084ee
Prathamesh Adate <prathameshadate05@gmail.com>
2023-01-26 15:38:08 +0530

    Setup

---
+++ src/main/java/com/spring/stickeynotes/StickeyNotes/StickeyNotesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.stickeynotes.StickeyNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickeyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickeyNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/stickeynotes/StickeyNotes/StickeyNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.stickeynotes.StickeyNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickeyNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
